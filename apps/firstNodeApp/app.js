5f69a37c6facb2d415b7b810be1399474b5ad90a
Christopher Bateson <cbateson@conversantmedia.com>
2019-02-17 19:58:50 -0600

    finished basic tutorial

--- apps/firstNodeApp/app.js
+++ apps/firstNodeApp/app.js
@@ -1,22 +1,7 @@
 const http = require('http');
 
+const { requestHandler: routes } = require('./routes');
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  res.setHeader('Content-Type', 'text/html');
-
-  res.write(
-    `<html>
-      <head>
-        <title>My first Page</title>
-      </head>
-      <body>
-        <h1>Hello from my Node.js Server</h1>
-      </body>
-    </html>`
-  )
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3500);
-
